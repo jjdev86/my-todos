bc316a6c7a7abe0fdddb2f7d0231d482036bfbbe
Jose Valadez <vinmajj@gmail.com>
2019-09-03 18:12:43 -0700

    Add feature to allow to create todo on enter keypress

--- client/main.js
+++ client/main.js
@@ -1,29 +1,42 @@
 // Add evenListener to Add button
 let addButton = document.getElementById('add-todo');
 // add event listener
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', function () {
   let input = document.querySelector("input[type='text']");
   addTodo(input)
 });
 
+// add evenlistener on keypress
+let input = document.querySelector("input[type='text']");
+
+// handle event on keypress
+input.addEventListener('keypress', function (keyPressed) {
+  if (keyPressed.charCode === 13) {
+    // addTodo(this)
+    addTodo(this)
+  }
+})
+
 // Append message to <ul>
 const addTodo = (input) => {
-    let li = document.createElement('li');
-    // create edit button
-    let editBtn = document.createElement('button');
-    editBtn.innerText = 'edit';
-    editBtn.classList.add('btn', 'edit-btn');
-    editBtn.addEventListener('click', updateTodo);
-    // create delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'delete';
-    deleteBtn.classList.add('btn', 'delete-btn');
-    deleteBtn.addEventListener('click', deleteTodo);
-    let span = document.createElement('span');
-    let ul = document.querySelector('ul');
-    let newTodo = input.value;
-    input.value = '';
-    ul.appendChild(li).append(span, newTodo, editBtn, deleteBtn);
+  let li = document.createElement('li');
+  // create edit button
+  let editBtn = document.createElement('button');
+  editBtn.innerText = 'edit';
+  editBtn.classList.add('btn', 'edit-btn');
+  editBtn.addEventListener('click', updateTodo);
+  // create delete button
+  let deleteBtn = document.createElement('button');
+  deleteBtn.innerText = 'delete';
+  deleteBtn.classList.add('btn', 'delete-btn');
+  deleteBtn.addEventListener('click', deleteTodo);
+  let span = document.createElement('span');
+  let ul = document.querySelector('ul');
+  debugger;
+  let newTodo = input.value;
+
+  input.value = '';
+  ul.appendChild(li).append(span, newTodo, editBtn, deleteBtn);
 };
 
 const updateTodo = () => {
@@ -31,5 +44,5 @@ const updateTodo = () => {
 };
 
 const deleteTodo = () => {
-    console.log(this, 'delete');
+  console.log(this, 'delete');
 };
