1b6fe228ecc867241e8362bab6e4a0935e0a2580
Jose Valadez <vinmajj@gmail.com>
2019-10-15 22:16:30 -0700

    refactor entire app to use server and db

--- index.js
+++ index.js
@@ -1,64 +1,90 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3000;
 // const Todo = require('./server/database/db');
 
-const { newUser, addTodo, editTodo } = require('./server/database/db');
+const {
+  newUser,
+  addTodo,
+  editTodo,
+  deleteTodo,
+  isComplete,
+  allTodos,
+  incompleteTodos,
+  completedTodos,
+  deleteAllTodos
+} = require("./server/database/db");
 
 // console.log(__dirname,'/client/dist')
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname,'/client/dist')))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 // Create new user
-app.post('/new-user', async (req, res) => {
-    // console.log(req.body);
-    const user = await newUser(req.body);
-    res.status(202).send(user);
+app.post("/new-user", async (req, res) => {
+  // console.log(req.body);
+  const user = await newUser(req.body);
+  res.status(202).send(user);
 });
 
 // Get all user todos
-app.get('/all-todos', (req, res) => {
-  
+app.get("/all-todos:id", async (req, res) => {
+//   console.log(req.params, `BODY`);
+  const All = await allTodos(req.params);
+  res.status(200).send(All);
 });
 // Get all incomplete todos
-app.get('/incomplete-todos', (req, res) => {
-
+app.get("/incomplete-todos:id", async (req, res) => {
+    const incompletes = await incompleteTodos(req.params.id)
+    res.status(200).send(incompletes);
 });
 
 // Get all completed todos
-app.get('/completed-todos', (req, res) => {
-
+app.get("/completed-todos:id", async (req, res) => {
+    const completed = await completedTodos(req.params.id)
+    res.status(200).send(completed);
 });
 
 // Add new todo
-app.post('/add-todo', async (req, res) => {
-    // console.log(req.body)
-    const todo = await addTodo(req.body);
+app.post("/add-todo", async (req, res) => {
+  // console.log(req.body)
+  const todo = await addTodo(req.body);
 
-    res.status(200).send(todo);
+  res.status(200).send(todo);
 });
 // Edit todo
-app.patch('/edit-todo', async (req, res) => {
-    console.log(req.body)
-    const update = await editTodo (req.body);
-    res.status(200).send(update);
-
+app.patch("/edit-todo", async (req, res) => {
+  console.log(req.body);
+  const update = await editTodo(req.body);
+  res.status(200).send(update);
+});
+app.patch("/complete-todo", async (req, res) => {
+//   console.log(req.body, `in server file`);
+  const update = await isComplete(req.body);
+  res.status(200).send(update);
 });
 // Delete Todo
-app.delete('/remove-todo', (req, res) => {
+app.delete("/remove-todo", async (req, res) => {
+  console.log(req.body);
+  const deleted = await deleteTodo(req.body.id);
+  // res.status(200).send(JSON.stringify(req.body.id));
+  res.status(200).send(deleted);
+});
+
+app.delete("/remove-all-todos:id", async (req, res) => {
+    const deleteAll = await deleteAllTodos(req.params.id)
+    try {
+        res.status(200).send('OK');
+    } catch(err) {
+        res.send(err);
+    };
 
 });
 
-app.delete('/remove-all-todos', (req, res) => {
-
-});
-
-
-app.listen(port, () => console.log('listening on port ', port));
+app.listen(port, () => console.log("listening on port ", port));
--- server/database/db.js
+++ server/database/db.js
@@ -1,69 +1,129 @@
-const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://localhost/todos', {
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/todos", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   console.log('"we\'re connected!"');
 });
- 
+
 const TodoSchema = new mongoose.Schema({
   todo: String,
   isComplete: { type: Boolean, default: false },
   username: String,
   date: { type: Date, default: Date.now },
-  userId: String,
+  userId: String
 });
 
 const Users = new mongoose.Schema({
-    username: String,
-    password: String,
-    date: { type: Date, default: Date.now }
-})
-const Todo = mongoose.model('todo', TodoSchema);
-const User = mongoose.model('user', Users);
+  username: String,
+  password: String,
+  date: { type: Date, default: Date.now }
+});
+const Todo = mongoose.model("todo", TodoSchema);
+const User = mongoose.model("user", Users);
 
-const newUser = async (username) => {
-    let response = await User.insertMany(username)
-    try {
-        return response;
-    } catch(err) {
-        return err;
-    }
+const newUser = async username => {
+  let response = await User.insertMany(username);
+  try {
+    return response;
+  } catch (err) {
+    return err;
+  }
 };
 
-const addTodo = async (body) => {
-    const response = await Todo.insertMany(body);
-    try {
-        // console.log(response,  `inside server`);
-        return response;
-    } catch(err) {
-        return err;
-    }
+const addTodo = async body => {
+  const response = await Todo.insertMany(body);
+  try {
+    return response;
+  } catch (err) {
+    return err;
+  }
 };
 
-const editTodo = async (body) => {
-    const id = body._id;
-    console.log(body.todo, `before update`);
-    const todo = await Todo.findByIdAndUpdate(id, {todo: body.todo})
-    try {
-        const updated = await Todo.findById(id); 
-        console.log(updated)
-        return updated;
-    } catch(err) {
-        console.log(err)
-        return err;
-    }
-}; 
+const editTodo = async body => {
+  const id = body._id;
+  const todo = await Todo.findByIdAndUpdate(id, { todo: body.todo });
+  try {
+    const updated = await Todo.findById(id);
+    // console.log(updated)
+    return updated;
+  } catch (err) {
+    return err;
+  }
+};
+const isComplete = async todo => {
+  const id = todo._id;
+  const isdone = await Todo.findByIdAndUpdate(id, {
+    isComplete: todo.isComplete
+  });
+  try {
+    const updated = await Todo.findById(id);
+    return updated;
+  } catch (err) {
+    return err;
+  }
+};
 
+const deleteTodo = async id => {
+  const todo = await Todo.findByIdAndDelete(id);
+  try {
+    return todo;
+  } catch (err) {
+    return err;
+  }
+};
+
+const allTodos = async id => {
+
+  const all = await Todo.find({ userId: id.id });
+  try {
+    return all;
+  } catch (err) {
+    return err;
+  }
+};
+
+const incompleteTodos = async id => {
+  // find all todos with id
+  const todos = await Todo.find({ userId: id, isComplete: { $eq: false } });
+  try {
+    return todos;
+  } catch (err) {
+    return err;
+  }
+};
+
+const completedTodos = async id => {
+  const todos = await Todo.find({ userId: id, isComplete: { $eq: true } });
+  try {
+    return todos;
+  } catch (err) {
+    return err;
+  }
+};
+
+const deleteAllTodos = async id => {
+  const todos = await Todo.deleteMany({ userId: id });
+  try {
+    return todos;
+  } catch (err) {
+    return err;
+  }
+};
 
 module.exports = {
-    newUser,
-    addTodo,
-    editTodo
-}
-
+  newUser,
+  addTodo,
+  editTodo,
+  deleteTodo,
+  isComplete,
+  allTodos,
+  incompleteTodos,
+  completedTodos,
+  deleteAllTodos
+};
