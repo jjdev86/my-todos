f0d826b3b8caf2a5bd0ff48a67bdecfa68a680ec
Jose Valadez <vinmajj@gmail.com>
2019-11-13 18:50:02 -0800

    update to Auth0

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://ec2-18-217-171-43.us-east-2.compute.amazonaws.com/login'
+      process.env.AUTH0_CALLBACK_URL || 'http://ec2-18-217-171-43.us-east-2.compute.amazonaws.com/callback'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
